e8a1e08e9d8f80ab04863a4803b21d5f8cb8b24b
loganS475 <los0017@auburn.edu>
2024-11-08 08:10:33 -0600

    comments and bug fixes

--- WeatherApp.java
+++ WeatherApp.java
@@ -7,19 +7,24 @@ import java.net.URI;
 import javax.swing.JTable;
 import javax.swing.JScrollPane;
 import javax.swing.JFrame;
+import java.util.Arrays;
 class WeatherApp{
 
     public static void main(String[] args) throws Exception {
+        // this takes in an input for longitude and latitude
         Scanner scnr = new Scanner(System.in);
         System.out.println("What is the longitude and laditude of your location");
-        Integer longitude = scnr.nextInt();
-        Integer laditude =  scnr.nextInt();
+        System.out.println("Usage: <longitude> <laditude> please use doubles");
+        Double longitude = scnr.nextDouble();
+        Double laditude =  scnr.nextDouble();
         scnr.close();
 
+        // All of the URL constructors are depreceated so a URI is needed
         URI temp = new URI("https://api.openweathermap.org/data/3.0/onecall?lat="+laditude.toString()+"&lon="+longitude.toString()+"&exclude=minutely,hourly,daily,alerts&appid=f7c51f779b08d241bafbdc3feb374ed5&units=imperial");
 
         URL weatherData = temp.toURL();
-        
+
+        // sets up connection to API and variables necessary for reading
         HttpURLConnection connection = (HttpURLConnection) weatherData.openConnection();
         
         connection.setRequestMethod("GET");
@@ -28,6 +33,7 @@ class WeatherApp{
         StringBuffer data = new StringBuffer();
         String info;
 
+        //makes sure connection is good and processes data from API
         if(responseCode == HttpURLConnection.HTTP_OK){
 
             BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
@@ -44,58 +50,87 @@ class WeatherApp{
         }else{
             System.out.println("Error with response code");
         }
-
+        
         info = data.toString();
-        info.replace('{',' ');
-
+        //spilts API input into the rows and columns for table
         String [] split1 = info.split(",");
-        String [] catagories = new String [split1.length];
-        String [] numbers = new String [split1.length];
+        String [] columns = new String [split1.length];
+        String [] rows = new String [split1.length];
 
         for(int i=0;i<split1.length;i++){
             String [] temp1 = split1[i].split(":");
-            catagories[i] = temp1[0];
-            numbers[i]=temp1[1];
+            columns[i] = temp1[0];
+            rows[i]=temp1[1];
             
         }
-
-        for(int i=0;i<numbers.length;i++){
+        //removes any bracket notation from API
+        for(int i=0;i<rows.length;i++){
             int index;
             char [] temp2;
-            if(numbers[i].contains("}")){
-                index = numbers[i].indexOf("}");
-                temp2 = numbers[i].toCharArray();
+            if(rows[i].contains("}")){
+                index = rows[i].indexOf("}");
+                temp2 = rows[i].toCharArray();
                 temp2[index]=' ';
-                numbers[i] = temp2.toString();
+                rows[i] = new String(temp2);
             }
-            if(numbers[i].contains("{")){
-                index = numbers[i].indexOf("{");
-                temp2 = numbers[i].toCharArray();
+            if(rows[i].contains("{")){
+                index = rows[i].indexOf("{");
+                temp2 = rows[i].toCharArray();
                 temp2[index]=' ';
-                numbers[i] = temp2.toString();
+                rows[i] = new String(temp2);
             }
-            if(numbers[i].contains("[")){
-                index = numbers[i].indexOf("[");
-                temp2 = numbers[i].toCharArray();
+            if(rows[i].contains("[")){
+                index = rows[i].indexOf("[");
+                temp2 = rows[i].toCharArray();
                 temp2[index]=' ';
-                numbers[i] = temp2.toString();
+                rows[i] = new String(temp2);
             }
-            if(numbers[i].contains("]")){
-                index = numbers[i].indexOf("]");
-                temp2 = numbers[i].toCharArray();
+            if(rows[i].contains("]")){
+                index = rows[i].indexOf("]");
+                temp2 = rows[i].toCharArray();
                 temp2[index]=' ';
-                numbers[i] = temp2.toString();
+                rows[i] = new String(temp2);
             }
             
         }
 
-        String [][] stats = {numbers};
+        for(int i=0;i<columns.length;i++){
+            int index;
+            char [] temp2;
+            if(columns[i].contains("}")){
+                index = columns[i].indexOf("}");
+                temp2 = columns[i].toCharArray();
+                temp2[index]=' ';
+                columns[i] = new String(temp2);
+            }
+            if(columns[i].contains("{")){
+                index = columns[i].indexOf("{");
+                temp2 = columns[i].toCharArray();
+                temp2[index]=' ';
+                columns[i] = new String(temp2);
+            }
+            if(columns[i].contains("[")){
+                index = columns[i].indexOf("[");
+                temp2 = columns[i].toCharArray();
+                temp2[index]=' ';
+                columns[i] = new String(temp2);
+            }
+            if(columns[i].contains("]")){
+                index = columns[i].indexOf("]");
+                temp2 = columns[i].toCharArray();
+                temp2[index]=' ';
+                columns[i] = new String(temp2);
+            }
+        }
 
-        JTable table = new JTable(stats,catagories);
+        //table set up
+        String [][] stats = {rows};
+
+        JTable table = new JTable(stats,columns);
 
         JScrollPane scrollPane = new JScrollPane(table);
 
-        JFrame frame = new JFrame("Simple JTable Example");
+        JFrame frame = new JFrame("Weather data for"+longitude.toString()+" "+laditude.toString());
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.add(scrollPane);
         frame.setSize(300, 500);
